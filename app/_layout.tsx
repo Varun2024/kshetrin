2f91163a5459cf01e912a95921c317cf46ae1f4b
Varun2024 <marvellous079@gmail.com>
2025-12-04 13:35:40 +0530

    tabs setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,5 +27,8 @@
 import { Stack } from "expo-router";
 import "./globals.css";
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack >
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+  </Stack>;
 }
